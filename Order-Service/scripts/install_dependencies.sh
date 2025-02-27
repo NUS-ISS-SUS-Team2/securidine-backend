d264e4e3ca80e72fc8ecb3e745ec9cf79521b97b
Bao Zhong <foo.bao.zhong@gmail.com>
2025-02-27 19:02:03 +0800

    Update appspec.yml

--- Order-Service/scripts/install_dependencies.sh
+++ Order-Service/scripts/install_dependencies.sh
@@ -4,3 +4,20 @@ sudo amazon-linux-extras enable docker
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -aG docker ec2-user
+
+
+# Install AWS CodeDeploy agent
+sudo yum install -y ruby wget
+
+# Get AWS Region dynamically
+AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
+
+# Download and install CodeDeploy agent
+cd /home/ec2-user
+wget https://aws-codedeploy-${AWS_REGION}.s3.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+
+# Enable and start CodeDeploy agent
+sudo systemctl enable codedeploy-agent
+sudo systemctl start codedeploy-agent
