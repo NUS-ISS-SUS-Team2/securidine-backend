bee23b80a5b2d2890cff66255dd673c34ec7b210
Bao Zhong <foo.bao.zhong@gmail.com>
2025-03-21 00:46:58 +0800

    Start Container script IAM access

--- Order-Service/scripts/start_container.sh
+++ Order-Service/scripts/start_container.sh
@@ -13,8 +13,13 @@ aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS
 echo "Pulling latest Docker image..."
 docker pull 302125150179.dkr.ecr.ap-southeast-1.amazonaws.com/nus-iss-sus-team2/securidine:latest
 
+SECRET=$(aws secretsmanager get-secret-value --secret-id SecretsManagerIamKeys --query SecretString --output text)
+
+AWS_ACCESS_KEY_ID=$(echo $SECRET | jq -r .AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY=$(echo $SECRET | jq -r .AWS_SECRET_ACCESS_KEY)
+
 echo "Running Docker container..."
-docker run -e AWS_ACCESS_KEY_ID=AKIAUMWAP57RX4YIIZ4N \
-           -e AWS_SECRET_ACCESS_KEY=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 \
+docker run -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+           -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
            -e AWS_DEFAULT_REGION=ap-southeast-1 \
            -d -p 8080:8080 --name order-service 302125150179.dkr.ecr.ap-southeast-1.amazonaws.com/nus-iss-sus-team2/securidine:latest
