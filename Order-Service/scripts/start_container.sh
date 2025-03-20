9722f09396ff294304e76b21b4ef931a30aa8850
Bao Zhong <foo.bao.zhong@gmail.com>
2025-03-21 00:05:17 +0800

    Encrypt DB values and hmac column test

--- Order-Service/scripts/start_container.sh
+++ Order-Service/scripts/start_container.sh
@@ -14,7 +14,7 @@ echo "Pulling latest Docker image..."
 docker pull 302125150179.dkr.ecr.ap-southeast-1.amazonaws.com/nus-iss-sus-team2/securidine:latest
 
 echo "Running Docker container..."
-docker run -e AWS_ACCESS_KEY_ID=your-access-key-id \
-           -e AWS_SECRET_ACCESS_KEY=your-secret-access-key \
+docker run -e AWS_ACCESS_KEY_ID=AKIAUMWAP57RX4YIIZ4N \
+           -e AWS_SECRET_ACCESS_KEY=dSbhmWCAfsC8aEmtG+Y5Ti82nWg5DfPdkunaMD6+ \
            -e AWS_DEFAULT_REGION=ap-southeast-1 \
            -d -p 8080:8080 --name order-service 302125150179.dkr.ecr.ap-southeast-1.amazonaws.com/nus-iss-sus-team2/securidine:latest
