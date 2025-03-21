deeeb2e2dc24c5116b5d6a841549f359736d15db
Bao Zhong <foo.bao.zhong@gmail.com>
2025-03-21 20:38:38 +0800

    AES Util change

--- Order-Service/src/main/java/com/SecuriDine/OrderService/Util/AESUtil.java
+++ Order-Service/src/main/java/com/SecuriDine/OrderService/Util/AESUtil.java
@@ -2,25 +2,16 @@ package com.SecuriDine.OrderService.Util;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class AESUtil {
     private static final String ALGORITHM = "AES";
-    private static byte[] SECRET_KEY;
-
-    // Static block to initialize the key from AWS Secrets Manager
-    static {
-        try {
-            String keyString = SecretManagerUtil.getAESKey(); // Fetch from Secrets Manager
-            SECRET_KEY = keyString.getBytes(); // Convert to byte array
-        } catch (Exception e) {
-            throw new RuntimeException("Error retrieving AES key from Secrets Manager", e);
-        }
-    }
 
     public static String encrypt(String data) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKey secretKey = new SecretKeySpec(SECRET_KEY, ALGORITHM);
+        SecretKey secretKey = getSecretKey();
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] encryptedBytes = cipher.doFinal(data.getBytes());
         return Base64.getEncoder().encodeToString(encryptedBytes);
@@ -28,9 +19,14 @@ public class AESUtil {
 
     public static String decrypt(String encryptedData) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKey secretKey = new SecretKeySpec(SECRET_KEY, ALGORITHM);
+        SecretKey secretKey = getSecretKey();
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
         return new String(cipher.doFinal(decodedBytes));
+    }
+
+    private static SecretKey getSecretKey() throws Exception {
+        String key = SecretManagerUtil.getAESKey();
+        return new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
     }
 }
