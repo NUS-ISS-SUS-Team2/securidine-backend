3389d407b72b8ab6c22841c3d3194f627ac6e6af
Bao Zhong <foo.bao.zhong@gmail.com>
2025-04-06 20:07:57 +0800

    Fix restaurantID error

--- Order-Service/src/main/java/com/SecuriDine/OrderService/Repository/RestaurantRepository.java
+++ Order-Service/src/main/java/com/SecuriDine/OrderService/Repository/RestaurantRepository.java
@@ -1,12 +1,19 @@
 package com.SecuriDine.OrderService.Repository;
 
 import com.SecuriDine.OrderService.Entity.Restaurant;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     // Custom query example (if needed later)
-    Restaurant findById(String name);
+	@Query("SELECT r FROM Restaurant r WHERE r.restaurantId = :restaurantId")
+    Optional<Restaurant> findById(@Param("restaurantId")Long id);
+
 }
